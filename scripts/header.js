a0642f548c7023bc391494eb1e077dc1e682a3a3
Daniel Correia <130774958+DanielJavaprogrammer@users.noreply.github.com>
2024-11-08 17:22:09 -0300

    ok

--- scripts/header.js
+++ scripts/header.js
@@ -1,6 +1,6 @@
 // Script para abrir e fechar menu no modo mobile
 document.querySelector("#icone-abrir").addEventListener("click", function () {
-    const menu = document.querySelector("nav#mobile #mobile_menu")
+    const menu = document.querySelector("nav#mobile #mobile_menu");
 
     if (menu.style.opacity != "1") {
         menu.style.opacity = "1";
@@ -9,19 +9,24 @@ document.querySelector("#icone-abrir").addEventListener("click", function () {
         menu.style.opacity = "0";
         menu.style.pointerEvents = "none";
     }
-})
+});
 
 // Script para abrir a janela do Caju
 document.querySelector(".caju").addEventListener("click", function () {
     location.href = "../code/beneficioCaju.html";
-})
+});
 
 const conexa = document.querySelector(".conexa");
 conexa.addEventListener("click", () => {
     location.href = "../code/beneficioConexa.html";
-})
+});
 
 const convenio = document.querySelector('.convenio');
 convenio.addEventListener("click", () => {
-    location.href = "../code/beneficioConvenio.html"
-})
+    location.href = "../code/beneficioConvenio.html";
+});
+
+const wellhub = document.querySelector(".gym-pass");
+wellhub.addEventListener("click", () => {
+    location.href = "../code/beneficioGymPass.html";
+});
